ce04320d0971447047525812de5a82e24644cb6c
bshibley19 <32138092+bshibley19@users.noreply.github.com>
2018-12-04 10:51:50 -0600

    Create gps.py

---
+++ gps.py
@@ -0,0 +1,20 @@
+from os import popen
+from time import sleep
+
+class GPS:
+    def __init__ (self, device = "/dev/serial0"):
+        self.device = device
+        self.rawReading = ""
+
+    def msgUpdate(self):
+        self.rawReading = popen("cat " + self.device).read()
+
+    def read(self):
+        self.msgUpdate()
+	return self.rawReading
+
+if __name__ == "__main__":
+    g = GPS()
+    r = g.read()
+    sleep(1)
+    print r
