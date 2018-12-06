ba403f91caa8f9fcf791a7a087ae85dbd9e40b46
alawrenz19 <alawrenz19@bsmschool.org>
2018-12-06 11:08:51 -0600

    code updates

--- vec_angle_length.py
+++ vec_angle_length.py
@@ -5,13 +5,21 @@ import time
 from math import pi
 from gps import GPS
 
-g = GPS()
-r = g.read()
+t = tcpClient("192.168.21.153")
+t.connect()
+data = t.recv()
+r = float(data)
+
+#data = data.split(",")
+#x = int(data[0])
+#y = int(data[1])
+#print(x, y)
+#g = GPS()
+#r = g.read()
 #r = '$GPRMC,154633.000,A,4457.4413,N,09320.5487,W,0.19,0.07,011118,,,A*7B'
 
 def data_transfer():
     sleep(2)
-
     start = r.find('$GPRMC')
     end = r.find(',0.', start)
     log = r[start:end]
@@ -30,6 +38,7 @@ def data_transfer():
     
     return lat, lon
 
+c = data_transfer()
 def data_read():
     start = c.find("44")
     end = c.find("',", start)  
@@ -45,60 +54,65 @@ def data_read():
     rlong = long - 9300
     trlong = rlong / 60
     longA =  0 - (trlong + 93)
+    return latA, longA
 
+
+coords_a = data_read()
+coords = coords_a.split(",")
+latA = int(coords[0])
+longA = int(coords[1])
 RPL.init() 
 
 motorL = 0
 motorR = 1
 #2000 for right 1000 for left
-latB = int(raw_input("Latitude > "))
-longB = int(raw_input("Longitude > "))
+latB = float(raw_input("Latitude > "))
+longB = float(raw_input("Longitude > "))
 
 def vec_trans_angle(x):
-	TurnTime = abs(x) / 20
-	#vectors are read in distances of 15.24 cm per unit
-	if x > 0:
-		move = time.time()
-		#turns left
- 		while time.time() < (move + TurnTime):
-	  		RPL.servoWrite(motorL, 0)
-			RPL.servoWrite(motorR, 2000)
-		if time.time() > (move + TurnTime):
-    			RPL.servoWrite(motorL, 0)
-    			RPL.servoWrite(motorR, 0)
+    TurnTime = abs(x) / 20
+    #vectors are read in distances of 15.24 cm per unit
+    if x > 0:
+        move = time.time()
+        #turns left
+ 	while time.time() < (move + TurnTime):
+	    RPL.servoWrite(motorL, 0)
+	    RPL.servoWrite(motorR, 2000)
+	if time.time() > (move + TurnTime):
+            RPL.servoWrite(motorL, 0)
+    	    RPL.servoWrite(motorR, 0)
 			
-	
-	if x < 0: 
-		move2 = time.time()
-		#turns right
-		while time.time() < (move2 + TurnTime):
-	  		RPL.servoWrite(motorL, 1000)
-			RPL.servoWrite(motorR, 0)
-		if time.time() > (move2 + TurnTime):
-    			RPL.servoWrite(motorL, 0)
-    			RPL.servoWrite(motorR, 0)
+    if x < 0: 
+        move2 = time.time()
+	#turns right
+	while time.time() < (move2 + TurnTime):
+	    RPL.servoWrite(motorL, 1000)
+	    RPL.servoWrite(motorR, 0)
+	if time.time() > (move2 + TurnTime):
+    	    RPL.servoWrite(motorL, 0)
+    	    RPL.servoWrite(motorR, 0)
 			
 
-def vec_length(x1,y1):
-  	v = vector(x1, y1) 
-  	length = v.mag()
-  	return length
+def vec_length(latA,longA):
+    v = vector(latA,longA) 
+    length = v.mag()
+    return length
 
 def vec_trans_length(magnitude):
   #robot goes 13.3 cm in 1 second
-  	Length = abs(magnitude) * 15.24
-  	Runtime = Length / 12.3
-  	move3 = time.time()
- 	while time.time() < (move3 + Runtime):
-    		RPL.servoWrite(motorL, 2000)
-		RPL.servoWrite(motorR, 1000)
-  	if time.time() > (move3 + Runtime):
-    		RPL.servoWrite(motorL, 0)
-    		RPL.servoWrite(motorR, 0)
+    Length = abs(magnitude) * 15.24
+    Runtime = Length / 12.3
+    move3 = time.time()
+    while time.time() < (move3 + Runtime):
+        RPL.servoWrite(motorL, 2000)
+	RPL.servoWrite(motorR, 1000)
+    if time.time() > (move3 + Runtime):
+    	RPL.servoWrite(motorL, 0)
+    	RPL.servoWrite(motorR, 0)
 		
 	
-def vec_combined(x1,y1,x2,y2):
-	vec_trans_angle(vec_angle(x1,y1,x2,y2))
-	vec_trans_length(vec_length(x2,y2))
+def vec_combined(latA,longA,latB,longB):
+    vec_trans_angle(vec_angle(latA,longA,latB,longB))
+    vec_trans_length(vec_length(latB,longB))
 	
 vec_combined(Numberx1, Numbery1, Numberx2, Numbery2)
